2566913b7987e28ee731598b1665aa5e4431be5c
linkismissing <zy0n.bear@protonmail.com>
2023-10-22 18:21:42 -0400

    add txID version a few more places

--- src/ui/network-ui.ts
+++ src/ui/network-ui.ts
@@ -2,11 +2,19 @@ import { NetworkName } from "@railgun-community/shared-models";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { Select } = require("enquirer");
 
+
+// UNUSED
 export const networkSelectionPrompt = async (): Promise<NetworkName> => {
   const prompt = new Select({
     header: " ",
     message: "Select Chain",
-    choices: ["Ethereum", "Binance Smart Chain", "Polygon", "Arbitrum"],
+    choices: [
+      "Ethereum",
+      "Binance Smart Chain",
+      "Polygon",
+      "Arbitrum",
+      "EthereumSepolia",
+    ],
   });
 
   const result = await prompt.run();
